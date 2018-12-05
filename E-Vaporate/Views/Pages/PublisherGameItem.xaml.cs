00a4941e7e50c2d8bf132a0aba6637693735def3
samirichards <12richards.sa@thestudio-liverpool.net>
2018-12-05 15:06:49 +0000

    Added coments to the code, as well as placed more category related code in using constructs to reduce overall memory usage.

--- E-Vaporate/Views/Pages/PublisherGameItem.xaml.cs
+++ E-Vaporate/Views/Pages/PublisherGameItem.xaml.cs
@@ -24,24 +24,33 @@ namespace E_Vaporate.Views.Pages
         public Game GameItem { get; set; }
         public PublisherGameItem(Game game)
         {
+            //Sets the datacontext to the game given to it so that the games information can be displayed with data binding
             GameItem = game;
             DataContext = GameItem;
+
             InitializeComponent();
-            var context = new EVaporateModel();
-            Lst_CategoryAssignment.ItemsSource = context.Categories.AsParallel();
-            List<Category> categories = new List<Category>();
             try
             {
-                foreach (var item in context.CategoryAssignments.Where(g => g.GameID == GameItem.GameID))
+                //Disposes of the connection to the database once the operation completes
+                using (var context = new EVaporateModel())
                 {
-                    categories.Add(context.Categories.Where(c => c.CategoryID == item.CategoryID).Single());
-                }
-                foreach (var item in categories)
-                {
-                    Lst_CategoryAssignment.SelectedItems.Add(item);
+                    //Sets the item source of the list to be the contents of the Categories table (being all the categories)
+                    Lst_CategoryAssignment.ItemsSource = context.Categories.AsParallel();
+                    List<Category> categories = new List<Category>();
+                    //Add each category to the list above where this game's ID is in the category assignment table
+                    foreach (var item in context.CategoryAssignments.Where(g => g.GameID == GameItem.GameID))
+                    {
+                        categories.Add(context.Categories.Where(c => c.CategoryID == item.CategoryID).Single());
+                    }
+                    //Select the items in the listview which are in this list also
+                    foreach (var item in categories)
+                    {
+                        Lst_CategoryAssignment.SelectedItems.Add(item);
+                    }
                 }
             }
-            catch (Exception) { }
+            catch (Exception)
+            { }
         }
 
         public void Dispose()
@@ -51,6 +60,7 @@ namespace E_Vaporate.Views.Pages
 
         private async void Btn_SaveChanges_Click(object sender, RoutedEventArgs e)
         {
+            //Show loading indicator on the foreground while the changes are saved
             (sender as Button).IsEnabled = false;
             Prog_SaveProgress.IsActive = true;
             Grd_Background.Visibility = Visibility.Visible;
@@ -63,6 +73,7 @@ namespace E_Vaporate.Views.Pages
             {
                 using (var context = new EVaporateModel())
                 {
+                    //Set all of the data for the current game to what is written on this page
                     context.Games.Where(g => g.GameID == GameItem.GameID).Single().Description = GameItem.Description;
                     context.Games.Where(g => g.GameID == GameItem.GameID).Single().Directory = GameItem.Directory;
                     context.Games.Where(g => g.GameID == GameItem.GameID).Single().Available = GameItem.Available;
@@ -75,6 +86,7 @@ namespace E_Vaporate.Views.Pages
                     MessageBox.Show("Changes saved sucessfully");
                 }
             }
+            //Display error message in the case that there is a problem
             catch (Exception a)
             {
                 MessageBox.Show("There was an issue saving your changes" + Environment.NewLine + a.Message);
@@ -82,6 +94,13 @@ namespace E_Vaporate.Views.Pages
             Prog_SaveProgress.IsActive = false;
             Btn_SaveChanges.IsEnabled = true;
             Grd_Background.Visibility = Visibility.Hidden;
+        }
+
+        //Validation on price tetbox, only numbers and periods can be entered
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex("[^0-9.]+$");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
